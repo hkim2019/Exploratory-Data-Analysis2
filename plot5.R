6f95b274eb9fb860ea321bb0b21a9c079465e1d8
hkim2019 <52400888+hkim2019@users.noreply.github.com>
2019-08-12 14:50:56 +0900

    Create plot5.R

---
+++ plot5.R
@@ -0,0 +1,11 @@
+##Question5
+SCCvehicle<-grepl(pattern = "vehicle", SCC$EISector, ignore.case = TRUE)
+SCCvehicleSCC <- SCC[SCCvehicle,]$SCC
+NEIvehicleSSC <- NEIdata[NEIdata$SCC %in% SCCvehicleSCC, ]
+NEIvehicleBaltimore <- subset(NEIvehicleSSC, fips == "24510")
+NIEvehicleBaltimoreTotEm<-aggregate(Emissions~year, NEIvehicleBaltimore, sum)
+g<-ggplot(aes(year, Emissions/10^5), data=NIEvehicleBaltimoreTotEm)
+g+geom_bar(stat="identity",fill="grey",width=0.75) +
+  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
